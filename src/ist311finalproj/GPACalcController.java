e908f7a8abed3eba5c0fcdaf79df175e958f5832
Kaung Khine <kaung.m.khine@gmail.com>
2020-04-29 18:38:20 -0400

    update gpa view

--- src/ist311finalproj/GPACalcController.java
+++ src/ist311finalproj/GPACalcController.java
@@ -8,13 +8,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class GPACalcController {
 
+    Scene previousScene;
+    
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -23,12 +29,57 @@ public class GPACalcController {
 
     @FXML // fx:id="goHomeButton"
     private Button goHomeButton; // Value injected by FXMLLoader
-    
-    Scene previousScene;
+
+    @FXML // fx:id="gpaCalcTable"
+    private TableView<?> gpaCalcTable; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classCreditCol"
+    private TableColumn<?, ?> classCreditCol; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classGradeCol"
+    private TableColumn<?, ?> classGradeCol; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classNameCol"
+    private TableColumn<?, ?> classNameCol; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classCredittxt"
+    private TextField classCredittxt; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classGradetxt"
+    private TextField classGradetxt; // Value injected by FXMLLoader
+
+    @FXML // fx:id="classNametxt"
+    private TextField classNametxt; // Value injected by FXMLLoader
+
+    @FXML // fx:id="addRowbtn"
+    private Button addRowbtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="removeRowBtn"
+    private Button removeRowBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="editRow"
+    private CheckBox editRow; // Value injected by FXMLLoader
+
+    @FXML // fx:id="calcGPAbtn"
+    private Button calcGPAbtn; // Value injected by FXMLLoader
+
+    @FXML
+    void clickAddRowBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickGPAbtn(ActionEvent event) {
+
+    }
 
     @FXML
     void clickHomeButton(ActionEvent event) {
-        //System.out.println("it works");
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 //        //Stage stage = (Stage)backButton.getScene().getWindow();
         if (null != previousScene){
@@ -42,9 +93,25 @@ public class GPACalcController {
         
     } 
 
+    @FXML
+    void clickRemRow(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert goHomeButton != null : "fx:id=\"goHomeButton\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert gpaCalcTable != null : "fx:id=\"gpaCalcTable\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classCreditCol != null : "fx:id=\"classCreditCol\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classGradeCol != null : "fx:id=\"classGradeCol\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classNameCol != null : "fx:id=\"classNameCol\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classCredittxt != null : "fx:id=\"classCredittxt\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classGradetxt != null : "fx:id=\"classGradetxt\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert classNametxt != null : "fx:id=\"classNametxt\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert addRowbtn != null : "fx:id=\"addRowbtn\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert removeRowBtn != null : "fx:id=\"removeRowBtn\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert editRow != null : "fx:id=\"editRow\" was not injected: check your FXML file 'GPACalcView.fxml'.";
+        assert calcGPAbtn != null : "fx:id=\"calcGPAbtn\" was not injected: check your FXML file 'GPACalcView.fxml'.";
 
     }
 }
